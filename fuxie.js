1398c3cee288473251a9018381080d43a1bec797
zhaolook <142658086+zhaolook@users.noreply.github.com>
2024-09-15 17:23:05 +0800

    Update fuxie.js

--- fuxie.js
+++ fuxie.js
@@ -320,7 +320,6 @@ function main(config) {
     "RULE-SET,PrimeVideo,Global Media",
     "RULE-SET,HBO,Global Media",
     "RULE-SET,China,DIRECT",
-    "DOMAIN-SUFFIX,emby.aca.best,Emby",
     "GEOIP,lan,DIRECT",
     "GEOIP,CN,DIRECT",
     "MATCH,Final"
