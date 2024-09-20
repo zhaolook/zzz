37f8fe700e7230dc24be0bf164b09ee98dc87d35
zhaolook <142658086+zhaolook@users.noreply.github.com>
2024-09-20 09:36:04 +0800

    Update fuxie.js

--- fuxie.js
+++ fuxie.js
@@ -327,7 +327,7 @@ function main(config) {
     "DOMAIN-SUFFIX,jsq.mooguu.top,Emby",
     "DOMAIN-SUFFIX,sfcj.org,EmbyDirect",
     "DOMAIN-SUFFIX,embymv.link,EmbyDirect",
-    "IP-CIDR,148.135.111.212/24,no-resolve,EmbyDirect",
+    "IP-CIDR,148.135.111.212/24,EmbyDirect",
     "MATCH,Final"
   ];
 
