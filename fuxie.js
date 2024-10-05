83e9a442d6f7e05af014a9e9f272b8a8aa9ca1e9
zhaolook <142658086+zhaolook@users.noreply.github.com>
2024-10-05 16:51:57 +0800

    Update fuxie.js

--- fuxie.js
+++ fuxie.js
@@ -335,6 +335,7 @@ function main(config) {
     "DOMAIN-SUFFIX,boaz.cf,EmbyDirect",
     "DOMAIN-SUFFIX,1024.name,EmbyDirect",
     "IP-CIDR,132.226.20.31/24,EmbyDirect",
+    "IP-CIDR,104.161.22.114/24,EmbyDirect",
     "MATCH,Final"
   ];
 
