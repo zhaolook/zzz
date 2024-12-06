14520256b25ff50f4564428c2d4c399e10569c54
zhaolook <142658086+zhaolook@users.noreply.github.com>
2024-12-06 19:04:11 +0800

    Update fuxie.js

--- fuxie.js
+++ fuxie.js
@@ -355,6 +355,7 @@ function main(config) {
     "DOMAIN-SUFFIX,nanflix.net,EmbyDirect",
     "DOMAIN-SUFFIX,worldline.space,EmbyDirect",
     "DOMAIN-SUFFIX,emby.prpr.cn.com,EmbyDirect",
+    "DOMAIN-SUFFIX,auroramedia.vip,EmbyDirect",
     "DOMAIN-SUFFIX,makima.online,Emby",
     "DOMAIN-SUFFIX,seamlessaccess.org,全球直连",
     "DOMAIN-SUFFIX,microsoft.com,全球直连",
