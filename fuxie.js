ca7e204056487869d85ce797d22bc6c0c20c8655
zhaolook <142658086+zhaolook@users.noreply.github.com>
2025-01-16 16:22:26 +0800

    Update fuxie.js

--- fuxie.js
+++ fuxie.js
@@ -365,6 +365,7 @@ function main(config) {
     "DOMAIN-SUFFIX,seamlessaccess.org,全球直连",
     "DOMAIN-SUFFIX,microsoft.com,全球直连",
     "IP-CIDR,132.226.20.31/24,EmbyDirect",
+    "IP-CIDR,15.235.220.182/24,EmbyDirect",
     "IP-CIDR,193.122.114.13/24,EmbyDirect",
     "IP-CIDR,129.154.57.174/24,EmbyDirect",
     "IP-CIDR,104.161.22.114/24,EmbyDirect",
