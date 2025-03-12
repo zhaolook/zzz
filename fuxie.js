6def04ec1f3b1a45caa83c827af9c3b6542ba595
zhaolook <142658086+zhaolook@users.noreply.github.com>
2025-03-12 21:24:22 +0800

    Update fuxie.js

--- fuxie.js
+++ fuxie.js
@@ -379,6 +379,7 @@ function main(config) {
     "DOMAIN-SUFFIX,outong-valve.com,EmbyDirect",
     "DOMAIN-SUFFIX,114514.quest,EmbyDirect",
     "DOMAIN-SUFFIX,saturday.video,EmbyDirect",
+    "DOMAIN-SUFFIX,nezhasu.com,EmbyDirect",
     "DOMAIN-SUFFIX,makima.online,Emby",
     "DOMAIN-SUFFIX,060618.me,Emby",
     "DOMAIN-SUFFIX,hannn.me,Emby",
